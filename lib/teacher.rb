f8462b975229e247a65f1c3696a997ac581cc65a
Jacqueline Lee <anginblu@gmail.com>
2017-11-30 06:03:18 +0000

    automatically backed up by learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -3,8 +3,11 @@ class Teacher < User
   attr_reader :knowledge
 
   def self.knowledge=(knowledge)
-    @knowledge = knowledge
+    @@knowledge = knowledge
   end
+
+  def self.knowledge
+    @@knowledge
 
   def teach
     @knowledge << self
